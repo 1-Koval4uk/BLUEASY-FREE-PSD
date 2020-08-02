a7344c6575e5cc10b965ab8f6369608c32642c00
1-Koval4uk <46053952+1-Koval4uk@users.noreply.github.com>
2020-08-02 13:25:57 +0300

    Added portfolio filter and twiter section + adaptive

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,14 @@ $(window).scroll(function(){
         $('.header').removeClass('header-active');
     };
 });
+
+$('.filter-button').click(function(){
+    var value =$(this).attr("data-filter");
+    if(value =='all'){
+        $('.filter').show('1000');
+    }
+    else{
+        $(".filter").not('.'+value).hide('1000');
+        $(".filter").filter('.'+value).show('1000');
+    }
+})
