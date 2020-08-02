f8b627a81baa8f10ea3f8e4e9e121b107bf42609
1-Koval4uk <46053952+1-Koval4uk@users.noreply.github.com>
2020-08-03 00:06:21 +0300

    Made last features section + adaptive + a few from myself

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,42 @@ $(window).scroll(function(){
     let wScroll = $(this).scrollTop();
     if (wScroll > 20) {
         $('.header').addClass('header-active');
+    }else if(wScroll > 50){
+        $('.serv').addClass('active');
     }
     else {
         $('.header').removeClass('header-active');
-    };
+    }
 });
+$(window).scroll(function(){
+    let wScroll = $(this).scrollTop();
+    if (wScroll > 300) {
+        $('.home').removeClass('active');
+        $('.serv').addClass('active');
+    }else{
+        $('.serv').removeClass('active');
+        $('.home').addClass('active');
+    }
+});
+$(window).scroll(function(){
+    let wScroll = $(this).scrollTop();
+    if (wScroll > 1000) {
+        $('.serv').removeClass('active');
+        $('.portf').addClass('active');
+    }else{
+        $('.portf').removeClass('active');
+    }
+});
+$(window).scroll(function(){
+    let wScroll = $(this).scrollTop();
+    if (wScroll > 1600) {
+        $('.portf').removeClass('active');
+        $('.feat').addClass('active');
+    }else{
+        $('.feat').removeClass('active');
+    }
+});
+
 
 $('.filter-button').click(function(){
     var value =$(this).attr("data-filter");
@@ -17,4 +48,4 @@ $('.filter-button').click(function(){
         $(".filter").not('.'+value).hide('1000');
         $(".filter").filter('.'+value).show('1000');
     }
-})
+});
